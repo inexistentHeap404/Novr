5e1ae07e014cf3bfe9d51daf1c699e7d64eefb2c
inexistentHeap404 <sanjayv0521@gmail.com>
2025-05-29 09:06:12 +0530

    landing page done

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -22,16 +22,47 @@ export default function Home(){
             <section className="browse_try">
                 <div className="ai_try">
                     <div className="card">
-                        <div className="background_card"></div>
-                        <div className="foreground_card"></div>
+                        <div className="background_card">
+                            <div className="foreground_card"></div>
+                        </div>
+                    </div>
+                    <div className="tagline_holder">
+                        <div className="tagline">Unleash your inner designer with novrVision – where <i>your</i> imagination meets <i>our</i> intelligence</div>
+                        <button>generate your own using novrVision</button>
                     </div>
                 </div>
                 <div className="browse_infinite">
                     <div className="card">
-                        <div className="background_card"></div>
-                        <div className="foreground_card"></div>
+                        <div className="background_card">
+                            <div className="foreground_card"></div>
+                        </div>
+                    </div>
+                    <div className="tagline_holder">
+                        <div className="tagline"><b>Straight from the Mind of AI</b> – explore T-shirt designs you’ve <i>never imagined</i>, but <i>instantly love</i>.</div>
+                        <button>Browse the Infinity store</button>
                     </div>
                 </div>
+            </section>
+            <section className="links_parent">
+                <div className="links_cols">
+                    <div className="links_rows">
+                        <ul className="link_rows_ul">
+                            <li className="link_header">Reach Us</li>
+                            <li className="link">Email Us</li>
+                            <li className="link">Instagram</li>
+                            <li className="link">Join out community in Reddit</li>
+                        </ul>
+                    </div>
+                    <div className="links_rows">
+                        <ul className="link_rows_ul">
+                            <li className="link_header">Quick Links</li>
+                            <li className="link">Home</li>
+                            <li className="link">novrVision</li>
+                            <li className="link">Your Wadrobe</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="hero_end">novr</div>
             </section>
         </>
     )
